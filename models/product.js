d45526ad74561417cefbd4191a68771c59d8b20a
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2023-12-25 11:52:52 +0530

    add sequelize

--- models/product.js
+++ models/product.js
@@ -1,33 +1,34 @@
-const db = require('../util/database');
-const Cart = require('./cart');
+const Sequelize = require('sequelize');
 
+const sequelize = require('../util/database');
 
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey:true
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull:false,
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull:false,
+    
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull:false,
+    
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull:false,
+    
   }
+})
 
-  save() {
-   return db.execute('INSERT INTO product (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', 
-    [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {
-    return db.execute('DELETE FROM product WHERE product.id = ?', [id]);
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM product');
-  }
-
-  static findById(id, cb){
-     return db.execute('SELECT * FROM product WHERE product.id = ?', [id]);
-  }
-}
-
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host : 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '2580@Ranvi'
-
+const sequelize = new Sequelize('node-complete','root','2580@Ranvi', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise()
+module.exports = sequelize;
